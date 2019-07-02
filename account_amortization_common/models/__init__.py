802dd49f0d593e239c316b9dab065dc53719e1dc
Andhitia Rama <andhitia.r@gmail.com>
2019-07-02 10:18:43 +0700

    [8.0.1.0.0] account_amortization_common

---
+++ account_amortization_common/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    account_amortization_type,
+    account_amortization_type_account_mapping,
+    account_amortization_common,
+    account_amortization_schedule_common,
+)
---
+++ account_amortization_common/models/account_amortization_type_account_mapping.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class AccountAmortizationTypeAccountMapping(models.Model):
+    _name = "account.amortization_type_account_mapping"
+    _description = "Amortization Type Account Mapping"
+
+    type_id = fields.Many2one(
+        string="Type",
+        comodel_name="account.amortization_type",
+    )
+    account_id = fields.Many2one(
+        string="Account",
+        comodel_name="account.account",
+    )
+    contra_account_id = fields.Many2one(
+        string="Contra Account",
+        comodel_name="account.account",
+    )
