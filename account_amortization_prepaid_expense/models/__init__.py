258db833995fe2278625248a9c317b0a18a1b6ca
Andhitia Rama <andhitia.r@gmail.com>
2019-07-02 12:07:53 +0700

    [8.0.1.0.0] account_amortization_prepaid_expense

---
+++ account_amortization_prepaid_expense/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    prepaid_expense_amortization,
+    prepaid_expense_amortization_schedule,
+)
