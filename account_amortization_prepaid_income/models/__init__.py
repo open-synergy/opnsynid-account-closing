a1c744b742e8a05423ae0d25d8d37fded3f3a937
mikevhe18 <viriyananda.michael@gmail.com>
2020-06-03 21:04:30 +0700

    [8.0.1.0.1] account_amortization_prepaid_income
    * implement multiple approval
    * implement cron to create amortization entry

--- account_amortization_prepaid_income/models/__init__.py
+++ account_amortization_prepaid_income/models/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import (
     prepaid_income_amortization,
     prepaid_income_amortization_schedule,
+    tier_definition,
 )
