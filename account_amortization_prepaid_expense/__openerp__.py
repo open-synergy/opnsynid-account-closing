ea3fdf9d8337ca8a4941ee8aba0592587e4b36b7
mikevhe18 <viriyananda.michael@gmail.com>
2020-06-03 21:04:17 +0700

    [8.0.1.0.1] account_amortization_prepaid_expanse
    * implement multiple approval
    * implement cron to create amortization entry

--- account_amortization_prepaid_expense/__openerp__.py
+++ account_amortization_prepaid_expense/__openerp__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Prepaid Expense Amortization",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.0.1",
     "category": "Accounting",
-    "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
@@ -19,6 +20,7 @@
         "data/base_sequence_configurator_data.xml",
         "data/base_workflow_policy_data.xml",
         "data/base_cancel_reason_configurator_data.xml",
+        "data/ir_cron.xml",
         "views/account_prepaid_expense_amortization_views.xml",
     ],
 }
